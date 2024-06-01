b5599e7dd39ded9feeb166bb0eb42def68c688fd
Anderson J. Santos <anderjsan@gmail.com>
2024-06-01 08:15:05 -0300

    [FIX] Aplying Reverse Sorted in Logs>. Using Fix as Test Matters.

--- app_swg.py
+++ app_swg.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from updater.updater import Updater
-from updater.app_swagger import App_swagger
+from updater.swagger import App_swagger
 
 if __name__ == "__main__":
     app = Flask(__name__)
